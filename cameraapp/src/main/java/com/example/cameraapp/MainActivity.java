9dbf4740c066df4af68d85213d9d0fcadece0ab8
xevi06 <xevi.armenteras@gmail.com>
2015-01-30 13:19:20 +0100

    camera app

---
+++ cameraapp/src/main/java/com/example/cameraapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.cameraapp;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Button camera;
+    private ImageView foto;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera = (Button) findViewById(R.id.button);
+        foto = (ImageView) findViewById(R.id.image);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takefoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takefoto.resolveActivity(getPackageManager())!=null){
+                    startActivityForResult(takefoto,REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
