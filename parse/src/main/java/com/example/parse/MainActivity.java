635bfff848e4d697e99409dec07311209132141c
xevi06 <xevi.armenteras@gmail.com>
2015-02-03 10:22:34 +0100

    parse-twiter, parse congif

--- parse/src/main/java/com/example/parse/MainActivity.java
+++ parse/src/main/java/com/example/parse/MainActivity.java
@@ -1,65 +1,123 @@
 package com.example.parse;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.parse.modules.delete_dialog;
+import com.parse.DeleteCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private Button button;
-    private ListView listView;
-    private EditText editText;
-    private ArrayList<String> restaurants;
+    private ListView mListView;
+    private EditText mEditText;
+    private Button mButton;
     private ArrayAdapter<String> adapter;
+
+    private ArrayList<String> data = new ArrayList<>();
+    private List<ParseObject> onlineData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "ZBmrOXlUMMmSXKObue13sjWbnVGGcrpwY0ZprZAN", "x0QLNKaYNQO9ubNCYX7qGEx9YkRQfN9XVxiyEWrP");
 
-        final ParseObject testObject = new ParseObject("Restaurant");
 
-        button = (Button) findViewById(R.id.button);
-        listView = (ListView) findViewById(R.id.list);
-        editText = (EditText) findViewById(R.id.edittext);
-        restaurants = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,restaurants);
-        listView.setAdapter(adapter);
+        mListView = (ListView) findViewById(R.id.list);
+        mEditText = (EditText) findViewById(R.id.edittext);
+        mButton = (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        fetchData();
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                data);
+        mListView.setAdapter(adapter);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(editText.getText().toString().equals("")){
-                    Toast.makeText(getApplicationContext(),"Edit empty text",Toast.LENGTH_SHORT).show();
-                }else{
-                    String s = editText.getText().toString();
-                    adapter.add(s);
-                    testObject.put("name", s);
-                    testObject.saveInBackground();
-                    editText.setText("");
-                }
+                final ParseObject parseObject = new ParseObject("Restaurants");
+                parseObject.put("name", mEditText.getText().toString());
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(getApplicationContext(), "Saved success", Toast.LENGTH_SHORT).show();
+                        adapter.add(mEditText.getText().toString());
+                        mEditText.setText("");
+                        onlineData.add(parseObject);
+                    }
+
+                });
             }
         });
 
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Remove?")
+                        .setTitle("Item");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ParseObject toDelete = onlineData.get(position);
 
+                        toDelete.deleteInBackground(new DeleteCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                onlineData.remove(position);
+                                data.remove(position);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
+            }
+        });
+    }
+
+    private void fetchData() {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Restaurants");
+        try {
+            onlineData = query.find();
+            for (ParseObject object : onlineData) {
+                data.add(object.getString("name"));
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -79,7 +137,6 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            adapter.clear();
             return true;
         }
 
---
+++ parse/src/main/java/com/example/parse/modules/delete_dialog.java
@@ -0,0 +1,33 @@
+package com.example.parse.modules;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+import com.example.parse.R;
+
+/**
+ * Created by Propietario on 02/02/2015.
+ */
+public class delete_dialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.delete_dialog)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
